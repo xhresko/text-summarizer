1f26bb6cd339f1829df06f81df236446135578c1
Juraj Hresko <juraj.hresko@gmail.com>
2010-08-20 12:25:15 +0200

    ocistenie stahovaneho textu od tagov a hluposti

--- getsomedata.py
+++ getsomedata.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
-
 import urllib.request
+import re
 
-archiv_url = r"http://www.lidovky.cz/ln_noviny.asp?c=A100819_000003_ln_noviny_sko"
+archiv_url = r"http://www.lidovky.cz/ln_noviny.asp?c=A100819_000022_ln_noviny_sko"
 
-beggining_tag = r"<!--FULLTEXTSTART-->"
+#beggining_tag = "<!--FULLTEXTSTART-->"
+beggining_tag =  r'<div class="text bbtext">'
 ending_tag = r"<!--INTEXTSTOP-->"
 
 urllib.request.urlretrieve(archiv_url, "001.txt")
@@ -14,12 +15,23 @@ f = open('001.txt', encoding='cp1250')
 
 content = f.read()
 
-print (content)
+#print (content)
 
 b = content.rfind(beggining_tag)
 
 e = content.rfind(ending_tag)
 
-print (b, e)
+#print (b, e)
 
-print (content[b:e])
+content = content[b:e]
+
+content = re.sub("<[^>]*>",' ',content) # oreze vsetky tagy z textu
+content = re.sub("Pokračování na straně [0-9]+",' ',content) # oreze vety, ktore zostali z tlacenej podoby novin
+content = re.sub("Dokončení ze strany [0-9]+",' ',content) # oreze vety, ktore zostali z tlacenej podoby novin
+content = re.sub("Více čtěte na straně [0-9]+",' ',content) # oreze vety, ktore zostali z tlacenej podoby novin
+
+content = re.sub(" čtk ",' ',content) # oreze pripadnu zmienku o CTK na konci clanku
+
+content = re.sub("[\*]+",' ',content) # oreze hviezdicky, ktore sa obcas v texte vyskytuju
+
+print(content)
