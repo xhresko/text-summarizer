565ebb607e102a79cb79711deaacebf9105442ad
Juraj Hresko <juraj.hresko@gmail.com>
2010-08-20 12:43:19 +0200

    upravy do tvaru class

--- getsomedata.py
+++ getsomedata.py
@@ -1,37 +1,52 @@
 #!/usr/bin/python3
 
+
 import urllib.request
 import re
 
-archiv_url = r"http://www.lidovky.cz/ln_noviny.asp?c=A100819_000022_ln_noviny_sko"
 
-#beggining_tag = "<!--FULLTEXTSTART-->"
-beggining_tag =  r'<div class="text bbtext">'
-ending_tag = r"<!--INTEXTSTOP-->"
+class LNTextRetriever:
 
-urllib.request.urlretrieve(archiv_url, "001.txt")
+    # premenne 
+    global archiv_url  
+    archiv_url = r"http://www.lidovky.cz/ln_noviny.asp?c=A100819_000022_ln_noviny_sko"
+    #beggining_tag = "<!--FULLTEXTSTART-->"
+    global beggining_tag 
+    beggining_tag =  r'<div class="text bbtext">'
+    global ending_tag 
+    ending_tag = r"<!--INTEXTSTOP-->"
+         
 
-f = open('001.txt', encoding='cp1250')
+    def get_article_text(self):
 
-content = f.read()
+        urllib.request.urlretrieve(archiv_url, "001.txt")
 
-#print (content)
+        f = open('001.txt', encoding='cp1250')
 
-b = content.rfind(beggining_tag)
+        content = f.read()
 
-e = content.rfind(ending_tag)
+        #print (content)
 
-#print (b, e)
+        b = content.rfind(beggining_tag)
 
-content = content[b:e]
+        e = content.rfind(ending_tag)
 
-content = re.sub("<[^>]*>",' ',content) # oreze vsetky tagy z textu
-content = re.sub("Pokračování na straně [0-9]+",' ',content) # oreze vety, ktore zostali z tlacenej podoby novin
-content = re.sub("Dokončení ze strany [0-9]+",' ',content) # oreze vety, ktore zostali z tlacenej podoby novin
-content = re.sub("Více čtěte na straně [0-9]+",' ',content) # oreze vety, ktore zostali z tlacenej podoby novin
+        #print (b, e)
 
-content = re.sub(" čtk ",' ',content) # oreze pripadnu zmienku o CTK na konci clanku
+        content = content[b:e]
 
-content = re.sub("[\*]+",' ',content) # oreze hviezdicky, ktore sa obcas v texte vyskytuju
+        content = re.sub("<[^>]*>",' ',content) # oreze vsetky tagy z textu
+        content = re.sub("Pokračování na straně [0-9]+",' ',content) # oreze vety, ktore zostali z tlacenej podoby novin
+        content = re.sub("Dokončení ze strany [0-9]+",' ',content) # oreze vety, ktore zostali z tlacenej podoby novin
+        content = re.sub("Více čtěte na straně [0-9]+",' ',content) # oreze vety, ktore zostali z tlacenej podoby novin
 
-print(content)
+        content = re.sub(" čtk ",' ',content) # oreze pripadnu zmienku o CTK na konci clanku
+
+        content = re.sub("[\*]+",' ',content) # oreze hviezdicky, ktore sa obcas v texte vyskytuju
+
+        return content
+
+
+
+retriever = LNTextRetriever()
+print(retriever.get_article_text())
