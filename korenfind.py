492acb836daf72eb0c38c4669889bf63faa089b6
Juraj Hresko <juraj.hresko@gmail.com>
2010-08-26 09:45:49 +0200

    pridany pekny clanok a upraveny algoritmus na hladanie slov s rovnakym korenom - skuma aj aku cast slova tvori najdeny fragment (kvoli odfiltrovaniu trojpismennych predloziek)

--- korenfind.py
+++ korenfind.py
@@ -14,24 +14,29 @@ cleanset = set()
 for word in wordlist :
     cleanset.add(word.strip())
 
+used_words = set()
+
 for word in cleanset :
     #print(word)
-    if(len(word)>3) :        
+    if(len(word)>3 and word not in used_words) :        
         wordfamily = word.upper() + " - "
         matches = 0
         for other in cleanset :                          
             same_chars = 0
             min_len = len(other)
+            max_len = len(word)
             if len(word) <= len(other) :
                 min_len = len(word)                         
+                max_len = len(other)
             for x in range(0,min_len) :
                 if word[x] == other[x] :                    
                     same_chars += 1
                 else :
                     break
-            if same_chars > 4 :
+            if same_chars > 3 and (max_len - same_chars) < 4 :
                 wordfamily = wordfamily + "," + other
                 matches += 1
+                used_words.add(other)
         if matches > 2 :
             print(wordfamily)
 
