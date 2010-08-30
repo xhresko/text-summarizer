3308801fb267516605a215894cafe6e69dac3d63
Juraj Hresko <hresko@ecommerce.cz>
2010-08-30 15:40:09 +0200

    upravene formatovanie, pridany atribut encoding pri otvarani suboru pre spravnu funkcnost na win

--- getsomedata.py
+++ getsomedata.py
@@ -2,9 +2,10 @@
 # -*- coding: utf-8 -*-
 
 
-import urllib.request
+from datetime import date
+from datetime import timedelta
 import re
-from datetime import date,timedelta
+import urllib.request
 
 class LNTextRetriever:
 
@@ -13,12 +14,12 @@ class LNTextRetriever:
 
     archiv_url = r"http://www.lidovky.cz/ln_noviny.asp?c=A", r"_ln_noviny_sko"
     #beggining_tag = "<!--FULLTEXTSTART-->"
-    beggining_tag =  r'<div class="text bbtext">'
+    beggining_tag = r'<div class="text bbtext">'
     ending_tag = r"<!--INTEXTSTOP-->"
     debug = 1;
     
 
-    def get_article_text(self,article_url,name):
+    def get_article_text(self, article_url, name):
 
         name = str(name) + ".txt"	
         urllib.request.urlretrieve(article_url, "temp.txt")
@@ -28,23 +29,23 @@ class LNTextRetriever:
         b = content.rfind(beggining_tag)
         e = content.rfind(ending_tag)
         
-        if(debug) :
+        if(debug):
             print("URL - " + article_url)
             print("Name - " + name)
             print("Start - " + str(b) + "\nEnd - " + str(e))
             
-        if(b==-1 or e==-1):
+        if(b == -1 or e == -1):
             print("Arcticle num. " + name + " missing")
             return 0
         content = content[b:e]	
-        content = re.sub("<[^>]*>",' ',content) # oreze vsetky tagy z textu
-        content = re.sub("Pokračování na straně [0-9]+",' ',content) # oreze vety, ktore zostali z tlacenej podoby novin
-        content = re.sub("Dokončení ze strany [0-9]+",' ',content) # oreze vety, ktore zostali z tlacenej podoby novin
-        content = re.sub("Více čtěte na straně [0-9]+",' ',content) # oreze vety, ktore zostali z tlacenej podoby novin
+        content = re.sub("<[^>]*>", ' ', content) # oreze vsetky tagy z textu
+        content = re.sub("Pokračování na straně [0-9]+", ' ', content) # oreze vety, ktore zostali z tlacenej podoby novin
+        content = re.sub("Dokončení ze strany [0-9]+", ' ', content) # oreze vety, ktore zostali z tlacenej podoby novin
+        content = re.sub("Více čtěte na straně [0-9]+", ' ', content) # oreze vety, ktore zostali z tlacenej podoby novin
 
-        content = re.sub(" čtk ",' ',content) # oreze pripadnu zmienku o CTK na konci clanku
+        content = re.sub(" čtk ", ' ', content) # oreze pripadnu zmienku o CTK na konci clanku
 
-        content = re.sub("[\*]+",' ',content) # oreze hviezdicky, ktore sa obcas v texte vyskytuju
+        content = re.sub("[\*]+", ' ', content) # oreze hviezdicky, ktore sa obcas v texte vyskytuju
                 
         f.close()
         
@@ -54,13 +55,13 @@ class LNTextRetriever:
         
         return content
 
-    def get_lndate(self,daydate):
+    def get_lndate(self, daydate):
         year = str(daydate.year-2000).zfill(2)
         month = str(daydate.month).zfill(2)
         day = str(daydate.day).zfill(2)
-        return year+month+day
+        return year + month + day
 
-    def get_url(self,daydate,article_num):
+    def get_url(self, daydate, article_num):
         article_num = str(article_num).zfill(6)
         return archiv_url[0] + self.get_lndate(daydate) + '_' + article_num + archiv_url[1]
 
@@ -71,7 +72,7 @@ retriever = LNTextRetriever()
 daydate = date.today() - timedelta(1)
 #print(retriever.get_url(daydate,11))
 
-for i in range(1,30) :
+for i in range(1, 30):
     name = str(i).zfill(3)
     name = './sobota/test' + name
     retriever.get_article_text(retriever.get_url(daydate,i),name)
--- textutil.py
+++ textutil.py
@@ -4,28 +4,28 @@ import re
 
 class RatedWordgroup:
     
-    def __init__(self,wordgroup,rating):
+    def __init__(self, wordgroup, rating):
         self.wordgroup = wordgroup
         self.rating = rating
 
     def __str__(self):
-        result = str(self.wordgroup) +" - " + str(self.rating)        
+        result = str(self.wordgroup) + " - " + str(self.rating)
         return result
     
-    def __eq__(self,other):
+    def __eq__(self, other):
         return self.wordgroup.eq(other.wordgroup) and self.rating.eq(other.rating)
 
     def __hash__(self):
         return hash(self.__str__())
         
-def get_text_stats(text) :
+def get_text_stats(text):
     """Vrati slovnik - obsahuje slova a pocet ich vyskytov"""
     dictionary = dict()
-    for word in get_word_bag(text) :
-        if word not in dictionary :
-            dictionary.update({word : 1})
-        else :
-            dictionary.update({word : int(dictionary.get(word)) + 1})
+    for word in get_word_bag(text):
+        if word not in dictionary:
+            dictionary.update({word: 1})
+        else:
+            dictionary.update({word: int(dictionary.get(word)) + 1})
 
     #sortlist = list()
     #for item in dictionary.items() :
@@ -37,7 +37,7 @@ def get_text_stats(text) :
     #    print (item)
     return dictionary
 
-def get_word_bag(text) :
+def get_word_bag(text):
     """Zo zadaneho textu vracia "bag of words" - v lowercase """
     text = text.lower()
     text = re.sub("[^a-ž]+", " ", text)
@@ -45,12 +45,12 @@ def get_word_bag(text) :
     text = re.sub("[ ]+", " ", text)
     wordlist = text.split(" ")
     word_bag = list()
-    for word in wordlist :
+    for word in wordlist:
         word_bag.append(word.strip())
     print("The text contains " + str(len(word_bag)) + " words.")
     return word_bag
 
-def get_sentences(text,delimiter='.') :
+def get_sentences(text, delimiter='.'):
     """Zo zadaneho textu vrati zoznam obsahujuci vety."""
     text = re.sub("\n", " ", text)
     text = re.sub("[ ]+", " ", text)
@@ -60,7 +60,7 @@ def get_sentences(text,delimiter='.') :
     print("The text contains " + str(len(sentences)) + " sentences.")
     return sentences
 
-def create_word_groups(text) :
+def create_word_groups(text):
     """ Vrati slova zoskupene podla prvych pismen """
 
     word_bag = get_word_bag(text)
@@ -68,35 +68,35 @@ def create_word_groups(text) :
     word_groups = dict() # vysledna mnozina mnozin slov s rovnakym zaciatkom
     cleanset = set() # cisty zoznam slov
 
-    for word in word_bag : # slova sa zjednotia v mnozine
+    for word in word_bag: # slova sa zjednotia v mnozine
         cleanset.add(word.strip())
 
     used_words = set()
 
-    for word in cleanset :
+    for word in cleanset:
         #print(word)
-        if(len(word)>3 and word not in used_words) :
+        if(len(word) > 3 and word not in used_words):
             wordfamily = list()
             wordfamily.append(word)
             matches = 0
-            for other in cleanset :
+            for other in cleanset:
                 same_chars = 0
-                (min_len,max_len) = (len(word),len(other)) if len(word) <= len(other) else (len(other),len(word))
-                for x in range(0,min_len) :
-                    if word[x] == other[x] :
+                (min_len, max_len) = (len(word), len(other)) if len(word) <= len(other) else (len(other), len(word))
+                for x in range(0, min_len):
+                    if word[x] == other[x]:
                         same_chars += 1
-                    else :
+                    else:
                         break
-                if same_chars > 4 and (max_len - same_chars) < 4 :
+                if same_chars > 4 and (max_len - same_chars) < 4:
                     wordfamily.append(other)
                     matches += 1
                     used_words.add(other)
-            if matches > 1 :
-                word_groups.update({word[:4] : wordfamily })
+            if matches > 1:
+                word_groups.update({word[:4]: wordfamily})
     print("The text contains " + str(len(word_groups)) + " word groups. (CWG)")
     return word_groups
 
-def group_words(text) :
+def group_words(text):
     """ Vrati slova zoskupene podla prvych pismen """
 
     word_bag = get_word_bag(text)
@@ -105,40 +105,40 @@ def group_words(text) :
     used_words = set()
     word_groups = list()
 
-    for word in word_bag : # slova sa zjednotia v mnozine
+    for word in word_bag: # slova sa zjednotia v mnozine
         cleanset.add(word.strip())
 
-    for word in cleanset :
-        if word not in used_words :
+    for word in cleanset:
+        if word not in used_words:
             wordfamily = list()
             wordfamily.append(word)
-            if(len(word)>3) :
+            if(len(word) > 3):
                 used_words.add(word)
-                for other in cleanset :
-                    if other not in used_words :
+                for other in cleanset:
+                    if other not in used_words:
                         same_chars = 0
-                        (min_len,max_len) = (len(word),len(other)) if len(word) <= len(other) else (len(other),len(word))
-                        for x in range(0,min_len) :
-                            if word[x] == other[x] :
+                        (min_len, max_len) = (len(word), len(other)) if len(word) <= len(other) else (len(other), len(word))
+                        for x in range(0, min_len):
+                            if word[x] == other[x]:
                                 same_chars += 1
-                            else :
+                            else:
                                 break
-                        if same_chars > 4 and (max_len - same_chars) < 6 :
+                        if same_chars > 4 and (max_len - same_chars) < 6:
                             wordfamily.append(other)
                             used_words.add(other)
             word_groups.append(wordfamily)
     print("The text contains " + str(len(word_groups)) + " word groups. (GW)")        
     return word_groups
 
-def get_wordlist_rate(text) :
+def get_wordlist_rate(text):
     word_groups = group_words(content)
     word_bag = get_word_bag(content)
     rated_word_set = set()
-    for group in word_groups :
+    for group in word_groups:
         occur = 0
-        for word in group :
+        for word in group:
             occur += word_bag.count(word)
-        rated_word_set.add(RatedWordgroup(group,occur))
+        rated_word_set.add(RatedWordgroup(group, occur))
         #if(occur>5 and len(group[0]) > 4) :
         #    print(group)
         #    print(occur)
@@ -149,28 +149,28 @@ def get_wordlist_rate(text) :
     #        print(rwg)
     return rated_word_set
 
-def rate_sentences(text) :
+def rate_sentences(text):
     sentences = get_sentences(text)
     wordlist = get_wordlist_rate(text)
     rated = set()
     topwords = set()
-    sort_wg = sorted(wordlist, key = lambda group : group.rating, reverse=True)
+    sort_wg = sorted(wordlist, key=lambda group: group.rating, reverse=True)
     for rwg in sort_wg:        
         if rwg.rating > 4 and len(rwg.wordgroup[0]) > 4 and len(rwg.wordgroup) > 1:
-            for word in rwg.wordgroup :
+            for word in rwg.wordgroup:
                 topwords.add(word)
 
-    for sentence in sentences :
+    for sentence in sentences:
         rating = 0
-        for word in sentence.split(" ") :
-            if word.lower() in topwords :
+        for word in sentence.split(" "):
+            if word.lower() in topwords:
                 rating += 1
         rated.add((sentence, rating))
-        if rating > 1 :
-            print(rating)
+        if rating > 1:
+            #print(rating)
             print(sentence + ".")
                 
-f = open("test.txt")
+f = open("test.txt", encoding="utf8")
 content = f.read()
 #get_sentences(content)
 #get_text_stats(content)
