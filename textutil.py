b4aee7bf2097c577656e1cceee661c5139537be9
Juraj Hresko <juraj.hresko@gmail.com>
2010-09-15 21:38:06 +0200

    precistenie metod,upravy vah a pridany dalsi textovy vystup

--- textutil.py
+++ textutil.py
@@ -34,25 +34,6 @@ class RatedSentence:
 
     def __hash__(self):
         return hash(self.rating)
-    
-def get_text_stats(text) :
-    """Vrati slovnik - obsahuje slova a pocet ich vyskytov"""
-    dictionary = dict()
-    for word in get_word_bag(text) :
-        if word not in dictionary :
-            dictionary.update({word : 1})
-        else :
-            dictionary.update({word : int(dictionary.get(word)) + 1})
-
-    #sortlist = list()
-    #for item in dictionary.items() :
-    #    if len(str(item[0])) > 3 :
-    #        hash_item = (str(item[1]).zfill(6) + " " + str(item[0]))
-    #        sortlist.append(hash_item)
-    #sortlist.sort()
-    #for item in sortlist :
-    #    print (item)
-    return dictionary
 
 def get_word_bag(text) :
     """Zo zadaneho textu vracia "bag of words" - v lowercase """
@@ -77,42 +58,6 @@ def get_sentences(text,delimiter='.') :
     print("The text contains " + str(len(sentences)) + " sentences.")
     return sentences
 
-def create_word_groups(text) :
-    """ Vrati slova zoskupene podla prvych pismen """
-
-    word_bag = get_word_bag(text)
-
-    word_groups = dict() # vysledna mnozina mnozin slov s rovnakym zaciatkom
-    cleanset = set() # cisty zoznam slov
-
-    for word in word_bag : # slova sa zjednotia v mnozine
-        cleanset.add(word.strip())
-
-    used_words = set()
-
-    for word in cleanset :
-        #print(word)
-        if(len(word)>3 and word not in used_words) :
-            wordfamily = list()
-            wordfamily.append(word)
-            matches = 0
-            for other in cleanset :
-                same_chars = 0
-                (min_len,max_len) = (len(word),len(other)) if len(word) <= len(other) else (len(other),len(word))
-                for x in range(0,min_len) :
-                    if word[x] == other[x] :
-                        same_chars += 1
-                    else :
-                        break
-                if same_chars > 4 and (max_len - same_chars) < 4 :
-                    wordfamily.append(other)
-                    matches += 1
-                    used_words.add(other)
-            if matches > 1 :
-                word_groups.update({word[:4] : wordfamily })
-    print("The text contains " + str(len(word_groups)) + " word groups. (CWG)")
-    return word_groups
-
 def group_words(text) :
     """ Vrati slova zoskupene podla prvych pismen """
 
@@ -129,7 +74,7 @@ def group_words(text) :
         if word not in used_words :
             wordfamily = list()
             wordfamily.append(word)
-            if(len(word)>3) :
+            if(len(word)>2) :
                 used_words.add(word)
                 for other in cleanset :
                     if other not in used_words :
@@ -140,7 +85,7 @@ def group_words(text) :
                                 same_chars += 1
                             else :
                                 break
-                        if same_chars > 4 and (max_len - same_chars) < 6 :
+                        if same_chars > 3 and (max_len - same_chars) < 7 :
                             wordfamily.append(other)
                             used_words.add(other)
             word_groups.append(wordfamily)
@@ -166,22 +111,26 @@ def get_wordlist_rate(text) :
     #        print(rwg)
     return rated_word_set
 
-def rate_sentences(text,percentage=20,min_rating=0) :
+def rate_sentences(text,percentage=20,verbose=True) :
     result = str()
     sentences = get_sentences(text)
     wordlist = get_wordlist_rate(text)
     rated = list()
-    topwords = set()
+    topwords = list()
     sort_wg = sorted(wordlist, key = lambda group : group.rating, reverse=True)
     for rwg in sort_wg:
         if rwg.rating > 3 and len(rwg.wordgroup[0]) > 4 and len(rwg.wordgroup) > 1:
             weight = 1
             weight += rwg.rating/4
-            weight += len(rwg.wordgroup[0])/3
-            weight += len(rwg.wordgroup)/4
+            weight += len(rwg.wordgroup[0])/2
+            weight += len(rwg.wordgroup)/1
             for word in rwg.wordgroup :
-                topwords.add((word, weight))
-    print(topwords)
+                #topwords.add((word, weight))
+                topwords.append((weight,word))
+    if(verbose):
+        for tw in sorted(topwords):
+            print("(" + str(tw[0]) + ") - " + tw[1])
+   #print(topwords)
     position = 0
     for sentence in sentences :
         rating = 0
@@ -189,8 +138,8 @@ def rate_sentences(text,percentage=20,min_rating=0) :
         bag = get_word_bag(sentence)
         for word in bag :
             for record in topwords :
-                 if word.lower()==record[0]:
-                     rating += record[1]        
+                 if word.lower()==record[1]:
+                     rating += record[0]        
         if(rating>0 and len(bag) > 0):             
             rating = rating/((len(bag))/7)
         
@@ -203,8 +152,8 @@ def rate_sentences(text,percentage=20,min_rating=0) :
             #result += sentence + "."
             #print(str(position) + " " + sentence + ".")
             #print(rating)
-    for rs in rated:
-        print(rs)
+    #for rs in rated:
+    #    print(rs)
         
     sort_sentences = sorted(rated, key = lambda sen : sen.rating, reverse=True)
         
@@ -233,18 +182,9 @@ def rate_sentences(text,percentage=20,min_rating=0) :
     
     return result
 
-<<<<<<< HEAD
 #f = open("test02.txt")
 #content = f.read()
 #get_sentences(content)
 #get_text_stats(content)
 #get_wordlist_rate(content)
 #rate_sentences(content)
-=======
-f = open("test02.txt")
-content = f.read()
-get_sentences(content)
-get_text_stats(content)
-get_wordlist_rate(content)
-rate_sentences(content)
->>>>>>> d515c9d7b8aaee796b61cd70a60f4b988f5c0182
